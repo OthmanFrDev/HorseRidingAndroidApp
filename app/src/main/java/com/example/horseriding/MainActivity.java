7eb8f8feb82e0c8a897cbafa759298f6adbb16e7
ayoubnemmassi <ayoub.nemmassi@gmail.com>
2021-04-20 01:32:04 +0000

    volley

--- app/src/main/java/com/example/horseriding/MainActivity.java
+++ app/src/main/java/com/example/horseriding/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.horseriding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +20,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //test
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final TextView textView = (TextView) findViewById(R.id.text);
+// ...
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.43.169:45459/users";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+
 }
